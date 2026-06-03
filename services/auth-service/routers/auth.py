f02d6285d1994b87fc4abdb8417ebd1da2f0f4a5
Rodrigo Takano Romero <hig29@txstate.edu>
2026-06-03 14:58:06 -0600

    [DARA-13] added importations and imp of auth for requests and responses

--- services/auth-service/routers/auth.py
+++ services/auth-service/routers/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from schemas.auth import RegisterRequest, RegisterResponse
-from services.auth_service import register_user
+from schemas.auth import RegisterRequest, RegisterResponse, LoginRequest, LoginResponse
+from services.auth_service import register_user, login_user
 from database import get_db
 
 router = APIRouter(prefix="/auth", tags=["auth"])
@@ -11,3 +11,8 @@ router = APIRouter(prefix="/auth", tags=["auth"])
 def register(request: RegisterRequest, db: Session = Depends(get_db)):
     token = register_user(request, db)
     return RegisterResponse(access_token=token)
+
+@router.post("/login", response_model=LoginResponse, status_code=200)
+def login(request: LoginRequest, db: Session = Depends(get_db)):
+    token = login_user(request, db)
+    return LoginResponse(access_token=token)
