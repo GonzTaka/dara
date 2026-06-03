73064f333193796484bcd26846697d1e89ff7453
Rodrigo Takano Romero <hig29@txstate.edu>
2026-06-03 10:15:01 -0600

    [DARA-13] Add login_user service function

--- services/auth-service/services/auth_service.py
+++ services/auth-service/services/auth_service.py
@@ -1,8 +1,8 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
 from models.user import User
-from schemas.auth import RegisterRequest
-from utils.security import hash_password
+from schemas.auth import LoginRequest, RegisterRequest
+from utils.security import hash_password, verify_password 
 from utils.jwt import create_access_token
 
 
@@ -23,3 +23,19 @@ def register_user(request: RegisterRequest, db: Session) -> str:
     db.refresh(new_user)
 
     return create_access_token(user_id=new_user.id)
+
+def login_user(request: LoginRequest, db: Session) -> str:
+    user = db.query(User).filter(User.email == request.email).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No account found with this email.",
+        )
+
+    if not verify_password(request.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect password.",
+        )
+
+    return create_access_token(user_id=user.id)
