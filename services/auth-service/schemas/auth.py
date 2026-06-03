9d1feae3c5ea8d14089edee3cd47e91da31d5fdd
Rodrigo Takano Romero <hig29@txstate.edu>
2026-06-03 10:14:32 -0600

    [DARA-13] Add LoginRequest and LoginResponse schemas

--- services/auth-service/schemas/auth.py
+++ services/auth-service/schemas/auth.py
@@ -9,3 +9,12 @@ class RegisterRequest(BaseModel):
 class RegisterResponse(BaseModel):
     access_token: str
     token_type: str = "bearer"
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+class LoginResponse(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+    
