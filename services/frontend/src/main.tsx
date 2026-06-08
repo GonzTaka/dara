2f05c863ce37fc53fb840c01fd5c65278a7bdb55
Rodrigo Takano Romero <hig29@txstate.edu>
2026-06-07 19:54:54 -0500

    [DARA-13] Add React Router with login and register routes

--- services/frontend/src/main.tsx
+++ services/frontend/src/main.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import LoginPage from '@/pages/LoginPage'
+import RegisterPage from '@/pages/RegisterPage'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
