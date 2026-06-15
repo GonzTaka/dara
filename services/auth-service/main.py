003e84d16d5a2afbd9d5b6f788549a1f70f187b4
Rodrigo Takano Romero <hig29@txstate.edu>
2026-06-14 19:42:18 -0500

    [DARA-13] Add CORS middleware to allow frontend requests

--- services/auth-service/main.py
+++ services/auth-service/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from routers import auth
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title="auth-service",
     description="Handles authentication, users, and workspaces for DARA.",
     version="0.1.0",
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth.router)
